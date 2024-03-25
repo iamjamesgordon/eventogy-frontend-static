724705e5fa8f87d10f1292455207c33f095896ff
iamjamesgordon <james@neontribe.co.uk>
2024-03-25 18:44:29 +0000

    Updating blank Filter component to complete page structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import PageTitle from "@/components/globals/pageTitle";
 import Container from "@/components/layouts/container";
 import PostsList from "@/components/globals/postsList";
+import Filter from "@/components/globals/filter";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <PageTitle>Event Hub</PageTitle>
       <main className='mb-6'>
         <Container>
+          <Filter></Filter>
           <PostsList></PostsList>
         </Container>
       </main>
