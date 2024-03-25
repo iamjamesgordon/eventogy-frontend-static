c92958fea788bc2e81058194305787e7e8d99ea9
iamjamesgordon <james@neontribe.co.uk>
2024-03-25 17:48:19 +0000

    Adding Tailwind CSS to project

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            screens: {
+                xs: '480px',
+            },
+            colors: {
+                'eventogy-blue': '#1862F8',
+            },
+        },
+    },
+    plugins: [],
+};
