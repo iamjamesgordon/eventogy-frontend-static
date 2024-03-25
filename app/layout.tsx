2850033c5c3f3db0378246c80c8408fd4c3b90ff
iamjamesgordon <james@neontribe.co.uk>
2024-03-25 17:41:41 +0000

    Adding prettier to project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,21 @@
-import { Inter } from "next/font/google";
+import { Inter } from 'next/font/google';
 
-import "./globals.css";
+import './globals.css';
 
 const inter = Inter({
-  weight: ["100", "200", "300", "400", "500", "600", "700"],
-  subsets: ["latin"],
-  variable: "--font-inter",
+  weight: ['100', '200', '300', '400', '500', '600', '700'],
+  subsets: ['latin'],
+  variable: '--font-inter',
 });
 
 export const metadata = {
-  title: "Eventogy - Static",
-  description: "",
+  title: 'Eventogy - Static',
+  description: '',
 };
 
 export default async function RootLayout({ children }) {
   return (
-    <html lang="en" className={inter.variable}>
+    <html lang='en' className={inter.variable}>
       <body>{children}</body>
     </html>
   );
