03a21707a8ec845188b8f0c00d4a6dd5c4ec0d7a
iamjamesgordon <james@neontribe.co.uk>
2024-03-25 18:52:09 +0000

    Adding a layout and buttons for the filter component

--- components/globals/filter.tsx
+++ components/globals/filter.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import Button from './button';
 
 export default function Filter() {
     return (
-        <div>Filter</div>
+        <div className='block flex-col py-6 sm:flex sm:justify-between lg:flex-row'>
+            <div className='mb-4 flex flex-wrap align-middle lg:mb-0'>
+                <Button>Events</Button>
+                <Button>Templates</Button>
+                <Button>Archive</Button>
+            </div>
+            <div className='flex flex-wrap align-middle'>
+                <Button>Grid</Button>
+                <Button>List</Button>
+                <Button>Sliders</Button>
+                <Button>Create New</Button>
+            </div>
+        </div>
     );
 }
