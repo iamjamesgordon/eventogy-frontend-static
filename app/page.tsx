ace062c0f5256c7abba8eb9ff55d8cb438d9d19d
iamjamesgordon <james@neontribe.co.uk>
2024-03-25 18:42:39 +0000

    Adding a structure to the page with blank components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 import PageTitle from "@/components/globals/pageTitle";
+import Container from "@/components/layouts/container";
+import PostsList from "@/components/globals/postsList";
 
 export default function Home() {
   return (
     <>
       <PageTitle>Event Hub</PageTitle>
+      <main className='mb-6'>
+        <Container>
+          <PostsList></PostsList>
+        </Container>
+      </main>
     </>
   );
 }
