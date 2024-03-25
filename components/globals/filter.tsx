9cb2b33861258f22e9c03baee008def3f7b04c65
iamjamesgordon <james@neontribe.co.uk>
2024-03-25 19:37:22 +0000

    Adding a rounded variation of buttons
    Splitting the buttons into different variations for scalability and less duplicate code...

--- components/globals/filter.tsx
+++ components/globals/filter.tsx
@@ -1,19 +1,40 @@
 import React from 'react';
-import Button from './button';
+import ButtonRounded from './buttons/buttonRounded';
 
 export default function Filter() {
     return (
         <div className='block flex-col py-6 sm:flex sm:justify-between lg:flex-row'>
             <div className='mb-4 flex flex-wrap align-middle lg:mb-0'>
-                <Button>Events</Button>
-                <Button>Templates</Button>
-                <Button>Archive</Button>
+                <ButtonRounded
+                    label='Events'
+                    icon='calendar'
+                    iconPos='left'
+                    rounded='xl'
+                    active={true}
+                    styles='mr-2'
+                />
+                <ButtonRounded
+                    label='Templates'
+                    icon='template'
+                    iconPos='left'
+                    rounded='xl'
+                    styles='mr-2'
+                />
+                <ButtonRounded
+                    label='Archives'
+                    icon='archives'
+                    iconPos='left'
+                    rounded='xl'
+                />
             </div>
             <div className='flex flex-wrap align-middle'>
-                <Button>Grid</Button>
-                <Button>List</Button>
-                <Button>Sliders</Button>
-                <Button>Create New</Button>
+                <ButtonRounded
+                    label='Create New'
+                    iconPos='right'
+                    rounded='lg'
+                    active={true}
+                    styles='ml-2'
+                />
             </div>
         </div>
     );
